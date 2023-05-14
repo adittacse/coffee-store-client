403b3a5ca07483abe1134d3a5fb776525a26c316
Aditta Chakraborty <adittacse@gmail.com>
2023-05-14 19:57:53 +0600

    coffee data displayed from server in home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react';
 import './App.css';
 import {useLoaderData} from "react-router-dom";
+import CoffeeCard from "./components/CoffeCard/CoffeeCard.jsx";
 
 function App() {
     const coffees = useLoaderData();
 
     return (
-        <div className="App">
-            <h1 className="text-6xl text-purple-600">Coffee Store</h1>
-            <p>{coffees.length}</p>
+        <div className="App m-20">
+            <h1 className="text-6xl text-center text-purple-600 my-10">Coffee Store</h1>
+            <div className="grid md:grid-cols-2 gap-5">
+                {
+                    coffees.map(coffee => <CoffeeCard key={coffee._id} coffee={coffee}></CoffeeCard>)
+                }
+            </div>
         </div>
     )
 }
---
+++ src/components/CoffeCard/CoffeeCard.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const CoffeeCard = ({coffee}) => {
+    const {name, quantity, supplier, taste, category, details, photo} = coffee;
+    
+    return (
+        <div className="card card-side bg-base-100 shadow-xl">
+            <figure><img className="items-center" src={photo} alt="Movie"/></figure>
+            <div className="flex items-center justify-between w-full pr-4">
+                <div>
+                    <h2 className="card-title">Name: {name}</h2>
+                    <p>Quantity: {quantity}</p>
+                    <p>Suppler: {supplier}</p>
+                    <p>Taste: {taste}</p>
+                    <p>Category: {category}</p>
+                </div>
+                <div className="card-actions justify-end items-center">
+                    <div className="btn-group btn-group-vertical space-y-3">
+                        <button className="btn btn-active">View</button>
+                        <button className="btn">Edit</button>
+                        <button className="btn">Delete</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CoffeeCard;

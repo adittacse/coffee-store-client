91e00599191b7c5d97690f276b60adf1dcdc53cc
Aditta Chakraborty <adittacse@gmail.com>
2023-05-15 01:21:42 +0600

    coffee loaded realtime from database after deletion

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import {useLoaderData} from "react-router-dom";
 import CoffeeCard from "./components/CoffeCard/CoffeeCard.jsx";
 
 function App() {
-    const coffees = useLoaderData();
+    const loadedCoffees = useLoaderData();
+    const [coffees, setCoffees] = useState(loadedCoffees);
 
     return (
         <div className="App m-20">
             <h1 className="text-6xl text-center text-purple-600 my-10">Coffee Store</h1>
             <div className="grid md:grid-cols-2 gap-5">
                 {
-                    coffees.map(coffee => <CoffeeCard key={coffee._id} coffee={coffee}></CoffeeCard>)
+                    coffees.map(coffee => <CoffeeCard key={coffee._id} coffee={coffee}
+                    coffees={coffees} setCoffees={setCoffees}></CoffeeCard>)
                 }
             </div>
         </div>
